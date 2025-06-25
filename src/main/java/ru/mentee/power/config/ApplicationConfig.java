9ab62324d5d732ace198d9440aed097c8c9450c2
danila_plater <danilaplyter@gmail.com>
2025-06-25 12:14:11 +0300

    Update ApplicationConfig.java

--- src/main/java/ru/mentee/power/config/ApplicationConfig.java
+++ src/main/java/ru/mentee/power/config/ApplicationConfig.java
@@ -1,24 +1,27 @@
 /* @MENTEE_POWER (C)2025 */
 package ru.mentee.power.config;
 
-import java.io.IOException;
-import java.io.InputStream;
-import java.util.Properties;
 import lombok.extern.slf4j.Slf4j;
-import ru.mentee.power.exception.SASTException;
+
+import java.io.IOException;
+import java.util.Properties;
 
 @Slf4j
-public static final String APP_NAME = "app.name";
+public class ApplicationConfig implements DatabaseConfig, Overridable, Fileable {
+  public static final String APP_NAME = "app.name";
+  public static final String APP_VERSION = "app.version";
+  public static final String DB_URL = "db.url";
+  public static final String DB_USER = "db.user";
 
   private final DatabaseConfig dbConfig;
   private final Properties properties;
-  private final ru.mentee.power.config.SecureValidator validator;
+  private final SecureValidator validator;
 
   public ApplicationConfig(Properties properties, ConfigFilePath configFilePath)
       throws IOException, SASTException {
     this.dbConfig = new PostgresConfig(properties);
     this.properties = properties;
-    this.validator = new ru.mentee.power.config.SecureValidator(properties);
+    this.validator = new SecureValidator(properties);
     load(configFilePath.getAppMainConfigPath());
     validator.validate();
     try {
@@ -32,61 +35,49 @@ public static final String APP_NAME = "app.name";
     override();
   }
 
+  @Override
+  public String getUrl() {
+    return dbConfig.getUrl();
+  }
+
+  @Override
+  public String getUser() {
+    return dbConfig.getUser();
+  }
+
+  @Override
+  public String getPassword() {
+    return dbConfig.getPassword();
+  }
+
   public String getApplicationName() {
     return properties.getProperty(APP_NAME);
   }
 
-    public String getUrl() {
-        return dbConfig.getUrl();
-    }
+  public String getApplicationVersion() {
+    return properties.getProperty(APP_VERSION);
+  }
 
-    public String getUsername() {
-        return dbConfig.getUsername();
-    }
+  @Override
+  public void load(String path) throws IOException {
+    // Реализация загрузки конфигурации
+  }
 
-    public String getPassword() {
-        return dbConfig.getPassword();
-    }
+  @Override
+  public void override() {
+    // Реализация переопределения параметров из переменных окружения
+    System.getenv().forEach((key, value) -> {
+      if (properties.containsKey(key)) {
+        properties.setProperty(key, value);
+      }
+    });
+  }
 
-    public String getDriver() {
-        return dbConfig.getDriver();
-    }
+  public Properties getProperties() {
+    return properties;
+  }
 
-    public boolean getShowSql() {
-        return dbConfig.getShowSql();
-    }
-
-    @Override
-    public void load(String pathProperties) throws IOException {
-        try (InputStream input = getClass().getResourceAsStream(pathProperties)) {
-            if (input == null) {
-                throw new IOException("Файл не найден: %s".formatted(pathProperties));
-            }
-            properties.load(input);
-        }
-    }
-
-    @Override
-    public void override() {
-        String envUrl = System.getenv(DB_URL);
-        String envUsername = System.getenv(DB_USERNAME);
-        String envPassword = System.getenv(DB_PASSWORD);
-        String envDriver = System.getenv(DB_DRIVER);
-        if (envUrl != null) {
-            properties.setProperty(DB_URL, envUrl);
-            log.info("{} переопределен из Environment Variable", DB_USERNAME);
-        }
-        if (envUsername != null) {
-            properties.setProperty(DB_USERNAME, envUsername);
-            log.info("{} переопределен из Environment Variable", DB_USERNAME);
-        }
-        if (envPassword != null) {
-            properties.setProperty(DB_PASSWORD, envPassword);
-            log.info("{} переопределен из Environment Variable", DB_PASSWORD);
-        }
-        if (envDriver != null) {
-            properties.setProperty(DB_DRIVER, envDriver);
-            log.info("{} переопределен из Environment Variable", DB_DRIVER);
-        }
-    }
+  public void validate() throws SASTException {
+    validator.validate();
+  }
 }
